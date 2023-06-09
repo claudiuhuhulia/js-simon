dea46e5f5d8f7ee81780500326f37cbf4e7d9cae
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-09 19:31:12 +0200

    bonus 1

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const RandomArrayElement = document.getElementById('randomarray');
+const CountdownElement = document.getElementById('countdown');
 
 let RandomNumArray=[];
 let UserNumbArray=[];
-let i = 0;
+let includes = 0;
+let userarray=[]; 
+let CountDown = 10;
 
 function GetRandomNum(){
     let RandomArray=[];
@@ -21,24 +24,17 @@ function GetRandomNum(){
 function vanish(){
     RandomArrayElement.classList.add('d-none');
 }
-let userarray=[];
+
 function Promts(){
-    let userNumb;
     for(i = 0 ; i < 5 ; i++){
-        userNumb = parseInt(prompt(`Digita il ${i + 1} numero che hai visto`));
-        userarray += userNumb;
+        let userNumb = parseInt(prompt(`Digita il ${i + 1}° numero che hai visto`));
+        userarray.push(userNumb);
     }
     return userarray;
 }
 UserNumbArray= userarray;
 
 
-
-if(RandomNumArray.includes(UserNumbArray)){
-i++;
-
-}
-console.log(i);
 
 RandomNumArray = GetRandomNum();
 console.log(RandomNumArray);
@@ -50,7 +46,25 @@ for(i = 0 ; i < 5 ; i++){
 
  RandomArrayElement.innerHTML= string; 
 
-    setTimeout(vanish, 10000); 
-    setTimeout(Promts, 5000); 
+setTimeout(vanish, 10000); 
+setTimeout(() => {
+        UserNumbArray = Promts();
+        console.log(UserNumbArray);
+        
+    for (i = 0; i < 5; i++) {
+    if (RandomNumArray.includes(UserNumbArray[i])) {
+      includes++;
+    }
+    }
+        console.log(includes);
+      alert(`HAI INDOVINATO ${includes} NUMERI SU 5`);
+}, 11000);
 
-    console.log(UserNumbArray);
+const count = setInterval(() => {
+        CountdownElement.innerText= --CountDown;
+        if(CountDown === 0){
+            CountdownElement.innerText= '';
+            clearInterval(count);
+        }
+}, 1000);
+
--- readme.md
+++ readme.md
@@ -5,10 +5,12 @@ Visualizzare in pagina 5 numeri casuali diversi. Da lì parte un timer di 30 sec
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 - Creo una function che mi generi un array di 5 numeri casuali diversi tra loro.
-    - Dichiaro la costante array vuota.
-    - Utilizzo un **While** per generare 5 numeri casuali, controllo che siano diversi usando il **Do while**.
-- Dichiaro una stringa vuota.
-    - Utilizzando un **Ciclo for** inserisco gli elementi dell'array precedentemente creata nel DOM.
-    - Creo una function che aggiunga la classe '*d-none*' all'elemento del DOM.
-    - Creo un **setTimout** nella quale inserirò la funtion precedentemente creata.
-    - Creo una function che chieda all'utente i numeri che si ricorda.
+- Utilizzo un **While** per generare 5 numeri casuali, controllo che siano diversi usando il **Do while**.
+- Utilizzando un **Ciclo for** inserisco gli elementi dell'array precedentemente creata nel DOM.
+- Creo una function che aggiunga la classe '*d-none*' all'elemento del DOM.
+- Creo un **setTimout** nella quale inserirò la funtion precedentemente creata.
+- Creo una function che chieda all'utente i numeri che successivamente andranno inseriti all'interno di un'array.
+ - Inserisco la function precedentemente creata all'interno della **Timing function**.
+    - Creo un ciclo for che verifichi se i numeri inseriti dall'utente siano contenuti nell'array dei numeri random.
+    - Inserisco un alert che comunichi all'utente quanti numeri ha indovinato.
+- Inserisco un **setInterval** che mi faccia da countdown diminuendo una variabile precedentemente dichiarata.
